4aa108b7a7b9b9b2d705fab7eb323fbfd58bd850
Henry Agustin <henryagustin297@gmail.com>
2022-12-13 01:30:04 -0600

    Terminé el paso 2 del formulario

--- public/index.js
+++ public/index.js
@@ -4,7 +4,7 @@ const cardInput = document.querySelector("#cardDetails-number")
 const mmInput = document.querySelector("#cardDetails-date--mm")
 const yyInput = document.querySelector("#cardDetails-date--yy")
 const cvcInput = document.querySelector("#cardDetails-date--cvc")
-const btn = document.querySelector("#btn")
+const btn = document.querySelector("#Btn")
 
 const nameOutput = document.querySelector("#nameCard")
 const cardOutput = document.querySelector("#numberCard")
@@ -28,17 +28,37 @@ cvcInput.addEventListener("input", ()=>{
     cvcOutput.innerText = cvcInput.value 
 })
 btn.addEventListener("click", ()=>{
-    location.reload()
+    const cardDetail = document.querySelector("#cardDetail")
+    cardDetail.innerHTML = ""
+    const cardDETAILS = document.createElement("div")
+    const img = document.createElement("object")
+    img.data = "/interactive-card-details-form/assets/icon-complete.svg"
+    img.setAttribute("width", "100")
+    img.setAttribute("height", "100")
+    const h1 = document.createElement("h1")
+    const h1Text = document.createTextNode("THANK YOU!")
+    const h2 = document.createElement("h2")
+    const h2Text = document.createTextNode("We’ve added your card details")
+    const button = document.createElement("button")
+    const buttonText = document.createTextNode("Continue")
+
+    button.appendChild(buttonText)
+    button.classList.add("btn")
+    button.setAttribute("id", "btnFin")
+    h2.appendChild(h2Text)
+    h2.classList.add("h2Text")
+    h1.appendChild(h1Text)
+    h1.classList.add("h1Text")
+
+    cardDETAILS.append(img,h1,h2,button)
+    cardDETAILS.classList.add("cardDetails", "textCenter")
+    cardDetail.append(cardDETAILS)
+    cardDetail.setAttribute("id", "cardDetail")
+    const btnFin = document.querySelector("#btnFin")
+    btnFin.addEventListener("click", ()=>{
+    location.reload()})
 })
 
-// if(cvcInput > 0){
-//     cvcInput.addEventListener("input", addData(cvcInput,cvcOutput))
-// }
-
-// function addData(input, output){
-//     console.log("dfgdg");
-//     output.innerText = input.value
-// }
 
 
 
